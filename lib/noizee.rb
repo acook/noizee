f9dd60d0dc39bad83a998091d6522fea94064617
Anthony Cook <github@anthonymcook.com>
2019-06-15 22:04:39 -0500

    WIP: nonblocking keypress to pause and quit.

--- lib/noizee.rb
+++ lib/noizee.rb
@@ -11,9 +11,57 @@ module Noizee
     require_relative 'noizee/internal'
     Noizee::Internal.event "Noizee initialized."
 
+    Remedy::Keyboard.raise_on_control_c!
+    Remedy::ANSI.cursor.hide!
+
     while true do
       puts gestalt.pop if gestalt.listen
+      interact
+    end
+
+  rescue Remedy::Keyboard::ControlC
+    quit "Noizee user pressed Ctrl-C. Exiting."
+  ensure
+    Remedy::ANSI.cursor.show!
+  end
+
+  def interact
+    key = get_key
+
+    case key
+    when nil
+      return
+    when ?q
+      quit "Noizee user pressed 'q'. Exiting."
+    when ?p
+      Remedy::Keyboard.get # pause until keypress
     end
   end
 
+  def get_key
+    key = nil
+    input = raw_get
+    if input
+      key = Remedy::Keyboard.parse input
+    end
+    key
+  end
+
+  def raw_get
+    Remedy::Console.raw do
+      input = STDIN.read_nonblock(1)
+      if input == "\e" then
+        input << STDIN.read_nonblock(3) rescue nil
+        input << STDIN.read_nonblock(2) rescue nil
+      end
+      input
+    end
+  end
+
+  def quit message
+    gestalt.events.clear
+    Noizee::Internal.event message
+    puts gestalt.pop
+    exit 0
+  end
 end
